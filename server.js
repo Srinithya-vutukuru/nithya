981b76677e2bb2ddfba35c696618acb373c78723
Srinithya-vutukuru <srinithya.vutukuru@gmail.com>
2016-10-26 21:57:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-   res.sendFile('artlcle one requested will be served here');
+app.get('/article-one', function (req, res) {
+   res.send('artlcle one requested will be served here');
 });
-app.get('/article-two',function(req,res){
-   res.sendFile('artlcle two requested will be served here');
+app.get('/article-two', function (req, res) {
+   res.send('artlcle two requested will be served here');
 });
-app.get('/article-three',function(req,res){
-   res.sendFile('artlcle three requested will be served here');
+app.get('/article-three', function (req, res) {
+   res.send('artlcle three requested will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
