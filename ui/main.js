d7c432af8982729c7615a425e44842bd0c244e40
Srinithya-vutukuru <srinithya.vutukuru@gmail.com>
2016-12-14 18:27:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML = 'this is nithya';
 var img = document.getElementById('madi');
 var marginleft= 0;
 var moveright = function(){
-    marginleft += 15;
+    marginleft += 5;
     img.style.marginLeft = marginleft + 'px';
 };
 img.onclick = function(){
