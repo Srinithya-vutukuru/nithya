739c3c482eb5196eada7a45599520efb1aba35e9
Srinithya-vutukuru <srinithya.vutukuru@gmail.com>
2016-12-14 18:53:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,11 +81,7 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter += 1;
-    res.send(counter.toString());
-});
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -94,7 +90,7 @@ app.get('/:articleName', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) 
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
